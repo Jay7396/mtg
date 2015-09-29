4c720b85b94022ec26dd0770ee5634c17e5b310c
Jerry Wang <jay7396@gmail.com>
2015-09-29 23:10:50 +0800

    get web href

--- mtg.js
+++ mtg.js
@@ -1,48 +1,27 @@
 var http = require('http');
-var url = 'http://magiccards.info/al/en/232.html'
 var cheerio = require('cheerio')
 
-function filterEdition(editionHtml){
-	var $ = cheerio.load(editionHtml)
-	var edition = $('table').eq(2).find('small')
-	var editionArray = []
-	edition.each(function(){
-		var edt = $(this).text()
-		editionArray.push(edt)
+function filterMap(mapHtml){
+	var $ = cheerio.load(mapHtml)
+	var map = $('table').find('li').find('a')
+	var mapHref = []
+	map.each(function(){
+		var maps = $(this).attr('href')
+		var edition = 'magiccard.info' + maps
+		mapHref.push(edition)
 	})
-	return editionArray
+	return mapHref
 }
 
-function filterImg(html){
-	var $ = cheerio.load(html)
-	var img = $('table').eq(3).find('img').eq(0).attr('src')
-	console.log(img)
-}
 
-http.get(url, function(res){
-	var html = ''
-
-	res.on('data', function(data){
-		html += data
+http.get('http://magiccards.info/sitemap.html',function(res){
+	var mapHtml = null
+	res.on('data',function(data){
+		mapHtml += data
 	})
-
-	res.on('end', function(){
-		console.log(filterImg(html))
+	res.on('end',function(){
+		console.log(filterMap(mapHtml))
 	})
-}).on('error', function(){
-	console.log('error')
-})
-
-http.get('http://magiccards.info/sitemap.html', function(res){
-	var editionHtml = ''
-
-	res.on('data', function(data){
-		editionHtml += data
-	})
-
-	res.on('end', function(){
-		console.log(filterEdition(editionHtml))
-	})
-}).on('error', function(){
-	console.log('error')
+}).on('error',function(){
+	console.log(err)
 })
